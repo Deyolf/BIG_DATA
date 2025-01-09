88cacc483a20e1199015286083826b37ac1c6d91
Deyolf <colombofabio06@gmail.com>
2025-01-09 10:29:18 +0100

    aggiunta divisione in pagine e tasti per cambiare pagina

--- scripthome.js
+++ scripthome.js
@@ -1,5 +1,6 @@
-table = document.getElementById("table")
-table.innerHTML=`<div class="row text-center bg-primary text-white fw-bold">
+function load_data(a) {
+  table = document.getElementById("table")
+  table.innerHTML = `<div class="row text-center bg-primary text-white fw-bold">
       <div class="col">Anno</div>
       <div class="col">Cittadinanza</div>
       <div class="col">Genere</div>
@@ -28,27 +29,71 @@ table.innerHTML=`<div class="row text-center bg-primary text-white fw-bold">
       <div class="col">100 e oltre</div>
     </div>`
 
-fetch('https://colombofabio4id.altervista.org/Big%20Data/api.php')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json(); // Converti la risposta in JSON
-  })
-  .then(data => {
-    console.log('Dati ricevuti:', data);
+  fetch(`https://colombofabio4id.altervista.org/Big%20Data/api.php?id=${a}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json(); // Converti la risposta in JSON
+    })
+    .then(data => {
+      console.log('Dati ricevuti:', data);
 
-    data.forEach(ele => {
-        a=JSON.stringify(ele)
-        table.innerHTML+=`
+      data.forEach(ele => {
+        table.innerHTML += `
         <div class="row text-center">
-      <div class="col">${a}</div>
+      <div class="col">${ele.anno}</div>
+      <div class="col">${ele.cittadinanza}</div>
+      <div class="col">${ele.genere}</div>
+      <div class="col">${ele.quartiere}</div>
+      <div class="col">${ele._0}</div>
+      <div class="col">${ele._01_04}</div>
+      <div class="col">${ele._05_09}</div>
+      <div class="col">${ele._10_14}</div>
+      <div class="col">${ele._15_19}</div>
+      <div class="col">${ele._20_24}</div>
+      <div class="col">${ele._25_29}</div>
+      <div class="col">${ele._30_34}</div>
+      <div class="col">${ele._35_39}</div>
+      <div class="col">${ele._40_44}</div>
+      <div class="col">${ele._45_49}</div>
+      <div class="col">${ele._50_54}</div>
+      <div class="col">${ele._55_59}</div>
+      <div class="col">${ele._60_64}</div>
+      <div class="col">${ele._65_69}</div>
+      <div class="col">${ele._70_74}</div>
+      <div class="col">${ele._75_79}</div>
+      <div class="col">${ele._80_84}</div>
+      <div class="col">${ele._85_89}</div>
+      <div class="col">${ele._90_94}</div>
+      <div class="col">${ele._95_99}</div>
+      <div class="col">${ele._100_oltre}</div>
       </div>
         `
-    });
+      });
 
-  })
-  .catch(error => {
-    console.error('Errore nella richiesta:', error);
-  });
+    })
+    .catch(error => {
+      console.error('Errore nella richiesta:', error);
+    });
+}
+
+let rendernumero = document.getElementById("numpag")
+
+let num_pag=1
+
+rendernumero.innerHTML = num_pag
+
+function next_page(){
+  num_pag++;
+  load_data(num_pag);
+  rendernumero.innerHTML = num_pag
+}
+function previous_page(){
+  num_pag--;
+  load_data(num_pag);
+  rendernumero.innerHTML = num_pag
+}
+
+load_data(num_pag);
 
