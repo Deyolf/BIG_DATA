a7ede1b232399f06c1e9b44c1d0c468d9ec6e27b
Deyolf <colombofabio06@gmail.com>
2025-01-10 16:07:39 +0100

    Update server.php
    enablin the usage of id=0 to allow access to the whole dataset

--- server.php
+++ server.php
@@ -1,31 +1,32 @@
 <?php
-// Abilita CORS
-header('Access-Control-Allow-Origin: *'); // Permette richieste da qualsiasi origine
-header('Access-Control-Allow-Methods: GET, POST, OPTIONS'); // Specifica i metodi consentiti
-header('Access-Control-Allow-Headers: Content-Type'); // Permette gli header specificati
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json');
 
 $id = $_GET["id"];
 
-$offset=($id-1)*50;
-$last = $id * 50;
+$jsonFile = __DIR__ . '/DATA.json';
 
-// Percorso del file JSON
-$jsonFile = __DIR__ . '/DATA.json'; // Assicurati che "data.json" sia nella stessa cartella
-
-// Controlla se il file esiste
 if (file_exists($jsonFile)) {
-    // Legge il contenuto del file JSON
+
     $jsonContent = file_get_contents($jsonFile);
 
-    $jsonObject = json_decode($jsonContent);
+    if ($id != 0) {
 
-    $first50Items = array_slice($jsonObject, $offset, $last);
+        $offset = ($id - 1) * 50;
+        $last = $id * 50;
 
-    // Restituisce il contenuto del file
-    echo json_encode($first50Items);
+        if ($last > 3693) { $last = 3693; }
+
+        $jsonObject = json_decode($jsonContent);
+        $first50Items = array_slice($jsonObject, $offset, $last);
+
+        echo json_encode($first50Items);
+
+    } else { echo $jsonContent; }
+
 } else {
-    // Restituisce un errore se il file non esiste
     http_response_code(404);
     echo json_encode(['error' => 'File JSON non trovato']);
 }
